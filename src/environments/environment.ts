2450aaf33743bccdd688e6af3d8dd77b56c9f9e4
Robert <rob76c@gmail.com>
2026-03-27 16:54:06 -0400

    stripe and environment added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: false,
   apiUrl: "http://localhost:8080/api/order",
   pubKey: "pk_test_XxoaXlbBXU4SWrNuhxxE4NiP",
-  stripeKey: "sk_test_51Dqy99Ch5pPFGILgacyVbq7cLPtwTTGiuSpj1kCG3rczpwHX0ibLEurOcpDp4w3JuJoNMA1zwhp5Jg1IIRmEVD5I00oirV4TUy"
 }
